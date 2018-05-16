17b6e7c6ab4fce92d4d257e46358bca9866398fc
Kathy  Varble <kathyvarble@Kathys-MacBook-Pro.local>
2018-05-16 17:15:43 -0500

    adding...

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('California', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Monster', true);
