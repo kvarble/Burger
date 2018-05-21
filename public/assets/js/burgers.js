047f74f5bd1ec0fed89771ef10a877b89db48904
Kathy  Varble <kathyvarble@Kathys-MacBook-Pro.local>
2018-05-20 20:41:37 -0500

    finishing touches

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -21,7 +21,7 @@ $(function(){
         event.preventDefault();
         var newBurger = {
             burger_name: $("#burger").val().trim(),
-            devoured: $("[name=devoured]:checked").val().trim()
+            devoured: "0"//$("[name=devoured]:checked").val().trim()
         };
         $.ajax("/api/burgers", {
             type: "POST",
